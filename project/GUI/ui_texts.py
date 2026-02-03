ebf005c01978de73797c985c99a5759405c4fe06
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-02-03 15:04:07 +0100

    Move UI placeholder text to separate module

---
+++ project/GUI/ui_texts.py
@@ -0,0 +1,10 @@
+# project/GUI/ui_texts.py
+
+HELP_PLACEHOLDER = (
+    "Program do obliczania produkcji\n\n"
+    "1) Wczytaj maszyny – pobiera aktualne zlecenia z bazy danych.\n\n"
+    "2) Przelicz produkcję – wylicza, do kiedy potrwa produkcja (data i zmiana).\n\n"
+    "3) Generuj raport – tworzy raport zapotrzebowania pod oklejanie z bazy danych za pośredniztwem ZMDRS.\n\n"
+    "Opcjonalnie:\n"
+    "• Wczytaj plik – tryb awaryjny z Excela (.xlsx/.xls), gdy baza danych nie działa.\n"
+)
