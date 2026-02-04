ba8bd77de9aae1fe3a05e111f8b79a3d6eb88d7c
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-02-04 14:23:28 +0100

    Add indentation for text

--- project/GUI/ui_texts.py
+++ project/GUI/ui_texts.py
@@ -4,7 +4,7 @@ HELP_PLACEHOLDER = (
     "Program do obliczania produkcji\n\n"
     "1) Wczytaj maszyny – pobiera aktualne zlecenia z bazy danych.\n\n"
     "2) Przelicz produkcję – wylicza, do kiedy potrwa produkcja (data i zmiana).\n\n"
-    "3) Generuj raport – tworzy raport zapotrzebowania pod oklejanie z bazy danych za pośredniztwem ZMDRS.\n\n"
+    "3) Generuj raport – tworzy raport zapotrzebowania pod oklejanie z bazy danych \n\n za pośredniztwem ZMDRS.\n\n"
     "Opcjonalnie:\n"
     "• Wczytaj plik – tryb awaryjny z Excela (.xlsx/.xls), gdy baza danych nie działa.\n"
 )
